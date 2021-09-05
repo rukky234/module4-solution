88da8834d89f59a054a21758f45c378c1093e95a
Rukayat <rukkyrulz@gmail.com>
2021-09-05 16:15:30 -0500

    new on harder

--- harder/SpeakHello.js
+++ harder/SpeakHello.js
@@ -9,7 +9,7 @@
 var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var helloSpeaker.speakWord = "Hello";
+var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
